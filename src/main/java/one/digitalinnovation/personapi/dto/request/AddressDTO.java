a9618809163e16cd56ad1e4bcfb5b400b72e9c5b
Breno <brenodveronezi@gmail.com>
2022-01-19 13:58:34 -0300

    added address for person

--- src/main/java/one/digitalinnovation/personapi/dto/request/AddressDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/request/AddressDTO.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import one.digitalinnovation.personapi.entity.Address;
-import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -16,29 +14,24 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class PersonDTO {
+public class AddressDTO {
 
     private Long id;
 
     @NotEmpty
-    @Size(min = 2, max = 100)
-    private String firstName;
+    @Size(min = 10, max = 100)
+    private String streetAddress;
 
     @NotEmpty
-    @Size(min = 2, max = 100)
-    private String lastName;
+    @Size(min = 8, max = 9)
+    private int zipCode;
 
     @NotEmpty
-    @CPF
-    private String cpf;
-
-    private String birthDate;
-
-    @Valid
-    @NotEmpty
-    private List<PhoneDTO> phones;
-
+    @Size(min = 6, max = 25)
+    private String city;
 
     @NotEmpty
-    private List<AddressDTO> address;
+    @Size(min = 5, max = 20)
+    private String country;
+
 }
---
+++ src/main/java/one/digitalinnovation/personapi/entity/Address.java
@@ -0,0 +1,33 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String streetAddress;
+
+    @Column(nullable = false)
+    private int zipCode;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String country;
+
+}
